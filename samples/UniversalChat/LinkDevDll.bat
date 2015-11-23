ae030d14b359bf3e57e4edbc03e2bedddbd458f9
Esun Kim <veblush@gmail.com>
2015-11-23 16:13:01 +0900

    Add DevLinkDll to samples/UniversalChat

---
+++ samples/UniversalChat/LinkDevDll.bat
@@ -0,0 +1,8 @@
+SET LinkToDev=..\..\..\Akka.Interfaced\misc\LibDevLink\LinkToDev.py l
+
+%LinkToDev% .\Interface\UniversalChat.Interface.csproj
+%LinkToDev% .\Interface.SlimClient\UniversalChat.Interface.SlimClient.csproj
+%LinkToDev% .\Interface.SlimClient.Net35\UniversalChat.Interface.SlimClient.Net35.csproj
+%LinkToDev% .\Program.Client.Console\UniversalChat.Program.Client.Console.csproj
+%LinkToDev% .\Program.Client.Unity\Program.Client.Unity.CSharp.csproj
+%LinkToDev% .\Program.Server\UniversalChat.Program.Server.csproj
