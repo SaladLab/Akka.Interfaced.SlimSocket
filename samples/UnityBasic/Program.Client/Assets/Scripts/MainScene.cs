5bb99de4e153fe2c453addf78050ee1b43e38a8d
Esun Kim <veblush@gmail.com>
2016-05-27 17:42:07 +0900

    Update unity sample matched with 0.3.0

--- samples/UnityBasic/Program.Client/Assets/Scripts/MainScene.cs
+++ samples/UnityBasic/Program.Client/Assets/Scripts/MainScene.cs
@@ -10,67 +10,89 @@ using UnityBasic.Interface;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MainScene : MonoBehaviour
+public class MainScene : MonoBehaviour, IHelloWorldEventObserver
 {
     public Text LogText;
 
     void Start()
     {
+        ApplicationComponent.TryInit();
+
         var serializer = new PacketSerializer(
             new PacketSerializerBase.Data(
                 new ProtoBufMessageSerializer(new InterfaceProtobufSerializer()),
                 new TypeAliasTable()));
 
-        G.Comm = new Communicator(G.Logger, new IPEndPoint(IPAddress.Loopback, 5000),
-            _ => new TcpConnection(serializer, LogManager.GetLogger("Connection")));
-        G.Comm.Start();
+        var comm = new Communicator(G.Logger, new IPEndPoint(IPAddress.Loopback, 5000),
+                                    _ => new TcpConnection(serializer, LogManager.GetLogger("Connection")));
+        comm.TaskFactory = new SlimTaskFactory { Owner = ApplicationComponent.Instance };
+        comm.ObserverEventPoster = c => ApplicationComponent.Post(c, null);
+        comm.Start();
+        G.Comm = comm;
 
         StartCoroutine(ProcessTest());
     }
 
-    void Update()
-    {
-        // TODO: Observer
-    }
-
     IEnumerator ProcessTest()
     {
-        yield return new WaitForSeconds(1);
+        var entry = G.Comm.CreateRef<EntryRef>();
 
         WriteLine("Start ProcessTest");
         WriteLine("");
 
-        yield return StartCoroutine(ProcessTestCounter());
-        yield return StartCoroutine(ProcessTestCalculator());
-        yield return StartCoroutine(ProcessTestPedantic());
+        var t1 = entry.GetHelloWorld();
+        yield return t1.WaitHandle;
+        yield return StartCoroutine(ProcessHelloWorld(t1.Result));
+
+        var t2 = entry.GetCalculator();
+        yield return t2.WaitHandle;
+        yield return StartCoroutine(ProcessCalculator(t2.Result));
+
+        var t3 = entry.GetCounter();
+        yield return t3.WaitHandle;
+        yield return StartCoroutine(ProcessCounter(t3.Result));
+
+        var t4 = entry.GetPedantic();
+        yield return t4.WaitHandle;
+        yield return StartCoroutine(ProcessPedantic(t4.Result));
     }
 
-    IEnumerator ProcessTestCounter()
+    // Tests
+
+    IEnumerator ProcessHelloWorld(IHelloWorld helloWorld)
     {
-        WriteLine("*** Counter ***");
+        WriteLine("*** HelloWorld ***");
 
-        var counter = new CounterRef(new SlimActorRef(1), new SlimRequestWaiter(G.Comm, this), null);
+        // add observer
 
-        yield return counter.IncCounter(1).WaitHandle;
-        yield return counter.IncCounter(2).WaitHandle;
-        yield return counter.IncCounter(3).WaitHandle;
+        var observer = G.Comm.CreateObserver<IHelloWorldEventObserver>(this);
+        yield return helloWorld.AddObserver(observer).WaitHandle;
 
-        var t1 = counter.IncCounter(-1);
+        // make some noise
+
+        var t1 = helloWorld.SayHello("World");
         yield return t1.WaitHandle;
-        ShowResult(t1, "IncCount(-1)");
+        ShowResult(t1, "SayHello(Hello)");
 
-        var t2 = counter.GetCounter();
+        var t2 = helloWorld.SayHello("Dlrow");
         yield return t2.WaitHandle;
-        ShowResult(t2, "GetCounter");
+        ShowResult(t2, "SayHello(Dlrow)");
+
+        var t3 = helloWorld.GetHelloCount();
+        yield return t3.WaitHandle;
+        ShowResult(t3, "GetHelloCount()");
 
         WriteLine("");
     }
 
-    IEnumerator ProcessTestCalculator()
+    void IHelloWorldEventObserver.SayHello(string name)
+    {
+        WriteLine(string.Format("<- SayHello({0})", name));
+    }
+
+    IEnumerator ProcessCalculator(ICalculator calculator)
     {
         WriteLine("*** Calculator ***");
-
-        var calculator = new CalculatorRef(new SlimActorRef(2), new SlimRequestWaiter(G.Comm, this), null);
 
         var t1 = calculator.Sum(1, 2);
         yield return t1.WaitHandle;
@@ -91,11 +113,28 @@ public class MainScene : MonoBehaviour
         WriteLine("");
     }
 
-    IEnumerator ProcessTestPedantic()
+    IEnumerator ProcessCounter(ICounter counter)
+    {
+        WriteLine("*** Counter ***");
+
+        yield return counter.IncCounter(1).WaitHandle;
+        yield return counter.IncCounter(2).WaitHandle;
+        yield return counter.IncCounter(3).WaitHandle;
+
+        var t1 = counter.IncCounter(-1);
+        yield return t1.WaitHandle;
+        ShowResult(t1, "IncCount(-1)");
+
+        var t2 = counter.GetCounter();
+        yield return t2.WaitHandle;
+        ShowResult(t2, "GetCounter");
+
+        WriteLine("");
+    }
+
+    IEnumerator ProcessPedantic(IPedantic pedantic)
     {
         WriteLine("*** Pedantic ***");
-
-        var pedantic = new PedanticRef(new SlimActorRef(3), new SlimRequestWaiter(G.Comm, this), null);
 
         var t1 = pedantic.TestCall();
         yield return t1.WaitHandle;
@@ -124,6 +163,8 @@ public class MainScene : MonoBehaviour
         WriteLine("");
     }
 
+    // Utilities
+
     void WriteLine(string text)
     {
         LogText.text = LogText.text + text + "\n";
@@ -148,4 +189,5 @@ public class MainScene : MonoBehaviour
         else
             ShowResult((Task)task, name);
     }
+
 }
